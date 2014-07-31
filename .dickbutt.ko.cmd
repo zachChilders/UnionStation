6954664f45eb542b3c81969d368b1a96d1ba2181
zach <zach@debian>
2014-07-31 00:53:14 -0700

    added readme

--- .dickbutt.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zach/Desktop/UnionStation/dickbutt.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-4-common/scripts/module-common.lds --build-id  -o /home/zach/Desktop/UnionStation/dickbutt.ko /home/zach/Desktop/UnionStation/dickbutt.o /home/zach/Desktop/UnionStation/dickbutt.mod.o
